74d65829b86affd0f6862e4feac5137fe3e2c3e1
Nicklas <gitnicklas@pm.me>
2024-06-03 01:59:51 +0300

    bump

--- arch_install/msi_laptop/systemd_boot/install.sh
+++ arch_install/msi_laptop/systemd_boot/install.sh
@@ -56,11 +56,11 @@ create_swap_file() {
 install_system() {
     echo "Pacstrap installation..."
     pacstrap -K /mnt base base-devel linux linux-firmware ${cpu_manufacturer}-ucode \
+        kitty git
         # sway swaylock swayidle swaybg waybar wofi \
         # networkmanager network-manager-applet \
         # nm-connection-editor \
         # neovim vim vifm obsidian firefox nemo \
-        kitty git
     genfstab -U /mnt >> /mnt/etc/fstab
 }
 
