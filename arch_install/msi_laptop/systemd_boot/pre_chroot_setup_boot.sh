2d496065b308e5dd35d8d75d6a93d0ce38f26883
Nicklas <gitnicklas@pm.me>
2024-06-03 01:04:38 +0300

    bump

---
+++ arch_install/msi_laptop/systemd_boot/pre_chroot_setup_boot.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+### Create, format and mount partitions
+
+set -e
+
+# Script to create and format partitions on a specified device using parted
+swap_size=12G
+create_swapfile=true
+# root_size=30G
+
+cpu_manufacturer=intel
+# cpu_manufacturer=amd
+
+
+### Functions
+mount_partition() {
+    local partition=$1
+    local mount_point=$2
+
+    echo "Mounting $partition on $mount_point..."
+    if ! mount --mkdir $partition $mount_point; then
+        echo "Failed to mount $partition on $mount_point. Exiting."
+        exit 1
+    fi
+}
+
+partition_drive() {
+    echo "Partitioning new GPT partition table on $DEVICE..."
+    parted --script $DEVICE mklabel gpt \
+           mkpart primary fat32 1MiB 1025MiB \
+           set 1 esp on \
+           mkpart primary ext4 1025MiB 41025MiB \
+           mkpart primary ext4 41025MiB 100%
+    sleep 2  # Allow the kernel to recognize the new partitions
+}
+
+format_partitions() {
+    echo "Formatting the EFI partition..."
+    mkfs.fat -F 32 ${DEVICE}p1
+    echo "Formatting Linux filesystem partitions..."
+    mkfs.ext4 ${DEVICE}p2
+    mkfs.ext4 ${DEVICE}p3
+}
+
+mount_partitions() {
+    mount_partition ${DEVICE}p2 /mnt
+    mount_partition ${DEVICE}p1 /mnt/boot
+    mount_partition ${DEVICE}p3 /mnt/home
+}
+
+create_swap_file() {
+    echo "Creating swap file..."
+    mkswap -U clear --size $swap_size --file /mnt/swapfile
+}
+
+install_system() {
+    echo "Pacstrap installation..."
+    pacstrap -K /mnt base base-devel linux linux-firmware ${cpu_manufacturer}-ucode \
+             sway swaylock swayidle waybar wofi \
+             networkmanager network-manager-applet nm-connection-editor \
+             neovim vim vifm obsidian firefox nemo
+    genfstab -U /mnt >> /mnt/etc/fstab
+}
+
+### Main Script Execution
+set -e
+if [ -z "$1" ]; then
+    echo "Usage: $0 /dev/sdX"
+    exit 1
+elif [ ! -b "$1" ]; then
+    echo "Error: $1 is not a valid block device."
+    exit 1
+fi
+DEVICE=$1
+
+for PART in $(ls ${DEVICE}* 2>/dev/null); do
+    if mount | grep -q ${PART}; then
+        echo "Unmount ${PART} first."
+        exit 1
+    fi
+done
+
+partition_drive
+format_partitions
+mount_partitions
+[ "$create_swapfile" = true ] && create_swap_file
+install_system
+
+echo "Partitioning, formatting and mounting complete."
+echo "Initiating chroot setup..."
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cp "${SCRIPT_DIR}/post_chroot_setup.sh" /mnt/.
+arch-chroot /mnt /bin/bash /post_chroot_setup.sh
