dfac8e0084ddb046695bbe8c25dbaaa43f1f8293
EternalGoldenBraid <gitnicklas@protonmail.ch>
2024-08-28 10:52:09 +0300

    disabled luasnips for now. Issues with not recognising math env in markdown

--- .config/nvim/lua/plugins/luasnips.lua
+++ .config/nvim/lua/plugins/luasnips.lua
@@ -1,18 +1,17 @@
 return {
-  {
-  	"L3MON4D3/LuaSnip",
-  	-- follow latest release.
-  	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-  	-- install jsregexp (optional!).
-  	build = "make install_jsregexp"
-  },
-  {
-    "iurimateus/luasnip-latex-snippets.nvim",
-    dependencies = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
-    config = function()
-      require("luasnip-latex-snippets").setup( { use_treesitter = true } )
-      require("luasnip").config.setup { enable_autosnippets = true }
-    end,
-  }
-
+  -- {
+  -- 	"L3MON4D3/LuaSnip",
+  -- 	-- follow latest release.
+  -- 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  -- 	-- install jsregexp (optional!).
+  -- 	build = "make install_jsregexp"
+  -- },
+  -- {
+  --   "iurimateus/luasnip-latex-snippets.nvim",
+  --   dependencies = { "L3MON4D3/LuaSnip", "lervag/vimtex" },
+  --   config = function()
+  --     require("luasnip-latex-snippets").setup( { use_treesitter = false } )
+  --     require("luasnip").config.setup { enable_autosnippets = true }
+  --   end,
+  -- }
 }
